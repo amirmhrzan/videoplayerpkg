5702d931bed2312265f5cd96f5cfe89b39c9e289
amir <amir@remotecoach.fit>
2022-02-28 08:19:31 +0545

    corrected packages

--- lib/videoplayerPackage/videojs_controller.dart
+++ lib/videoplayerPackage/videojs_controller.dart
@@ -3,8 +3,8 @@
 import 'package:flutter/widgets.dart';
 
 import 'videojs_stub.dart'
-    if(dart.library.io) 'package:maeve_madden/videoplayerPackage/mobile_videojs_finder.dart'
-    if(dart.library.html) 'package:maeve_madden/videoplayerPackage/web_videojs_finder.dart';
+    if(dart.library.io) 'package:videoplayerpkg/videoplayerPackage/mobile_videojs_finder.dart'
+    if(dart.library.html) 'package:videoplayerpkg/videoplayerPackage/web_videojs_finder.dart';
 
 abstract class VideosJsController{
 
